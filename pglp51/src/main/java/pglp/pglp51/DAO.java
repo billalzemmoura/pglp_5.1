ec16fd62b09322d6e4bd912ef55609dad895b13f
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-04-13 18:00:11 +0200

    modification

--- pglp51/src/main/java/pglp/pglp51/DAO.java
+++ pglp51/src/main/java/pglp/pglp51/DAO.java
@@ -2,9 +2,9 @@ package pglp.pglp51;
 
 public interface DAO <T>{
 public T create(T obj);
-public T find(T obj);
+public boolean find(T obj);
 public T update(T obj);
 public T delete(T obj);
-public DAO <T> getDAO();
+
 
 }
