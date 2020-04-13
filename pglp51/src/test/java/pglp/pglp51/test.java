16046310340efd6cae18f17771c72e1e9aeba09d
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-04-13 18:11:22 +0200

    imlémentation des cas de tests pour les methodes CRUD

--- pglp51/src/test/java/pglp/pglp51/test.java
+++ pglp51/src/test/java/pglp/pglp51/test.java
@@ -1,18 +1,17 @@
 package pglp.pglp51;
 
-import lombok.Data;
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Date;
+import java.util.ArrayList;
+
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,20 +27,10 @@ public class test
 
 	}
 
-	@Test
-	public void testInstance() throws IOException, ClassNotFoundException {
-		byte[] serialized = serialize(personne);
-		Object deserialized = deserialize(serialized);
-
-		assertTrue(deserialized instanceof Personnels);
-		assertEquals(personne.getNom(), ((Personnels) deserialized).getNom());
-		assertEquals(personne.getPrenom(), ((Personnels) deserialized).getPrenom());
-		assertEquals(personne.getIdPersonnels(), ((Personnels) deserialized).getIdPersonnels());
-	}
-
+	
 	@Test
 	public void testSerealise() {
-		// Personnels personne=new Personnels.Builder(4, "BILL"," zemmoura").build();
+		
 		ObjectOutputStream oos = null;
 		ObjectInputStream ois = null;
 
@@ -53,16 +42,12 @@ public class test
 			final FileInputStream fichierIn = new FileInputStream("personne.ser");
 			ois = new ObjectInputStream(fichierIn);
 			Personnels personneDeserealisable = (Personnels) ois.readObject();
-			//assertEquals(personne, personneDeserealisable);
+			
 			assertEquals(personne.getNom(), (personneDeserealisable).getNom());
 			assertEquals(personne.getPrenom(), ( personneDeserealisable).getPrenom());
 			assertEquals(personne.getIdPersonnels(), personneDeserealisable.getIdPersonnels());
 			
-			System.out.println("Personne : ");
-			System.out.println("nom : " + personne.getNom());
-			System.out.println("prenom : " + personne.getPrenom());
-			System.out.println("l'identifiant : : " + personne.getIdPersonnels());
-			System.out.println("numéro de tél : " + personne.getTel());
+			
 		} catch (final java.io.IOException e) {
 			e.printStackTrace();
 		} catch (final ClassNotFoundException e) {
@@ -82,27 +67,101 @@ public class test
 
 	}
 
-	private static byte[] serialize(Object obj) throws IOException {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		ObjectOutputStream o = new ObjectOutputStream(b);
-		o.writeObject(obj);
-		return b.toByteArray();
-	}
-
-	private static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
-		ByteArrayInputStream b = new ByteArrayInputStream(bytes);
-		ObjectInputStream o = new ObjectInputStream(b);
-		return o.readObject();
-	}
-
 	/**
 	 * Create the test case
 	 *
 	 * @param testName name of the test case
 	 */
 	@Test
+	public void TestCreate(){
+		Personnels p5=new Personnels.Builder(5, "BILL", " zemmoura").build();
+		DAO<Personnels> pDAO=DAOFactory.getPersonnelDAO();
+		pDAO.create(p5);
+		assertTrue(pDAO.find(p5));
+	
+	}
+	
+	@Test
+	public void TestDelete(){
+		Personnels p5=new Personnels.Builder(5, "BILL", " zemmoura").build();
+		DAO<Personnels> pDAO=DAOFactory.getPersonnelDAO();
+		pDAO.create(p5);
+		pDAO.delete(p5);
+		assertFalse(pDAO.find(p5));
+	
+	}
+	
+	@Test
+	public void Testfind(){
+		Personnels p8=new Personnels.Builder(8, "BILL", " zemmoura").build();
+		Personnels p4=new Personnels.Builder(4, "bob", " bob").build();
+		DAO<Personnels> pDAO=DAOFactory.getPersonnelDAO();
+		pDAO.create(p4);
+		assertFalse(pDAO.find(p8));
+		assertTrue(pDAO.find(p4));
+		
+	
+	}
+
+	@Test
+	public void Testupdate(){
+		boolean isUpdate=false;
+		Personnels p9=new Personnels.Builder(9, "BILL", " zemmoura").build();
+		ArrayList<Personnels> load=new ArrayList<Personnels>();
+		DAO<Personnels> pDAO=DAOFactory.getPersonnelDAO();
+		pDAO.create(p9);
+		p9.setNom("ze");;
+		//System.out.println(p9.IdPersonnels);
+		pDAO.update(p9);
+		
+		load=personnelDAO.lire();
+		for (Personnels personnels : load) {
+		   if (personnels.getIdPersonnels()==9) {
+			isUpdate=true;
+		}
+		   System.out.println(personnels.getNom());
+		
+		}
+		//find ne doit pas trouver l'objet p9 car elle l'es détecte par leurs identifaints 
+		
+		assertTrue(isUpdate);
+	
+	}
+
+	
+	@Test
+	public void TestPersonelDAO() {
+		
+
+	    ArrayList<Personnels> load=new ArrayList<Personnels>();
+	    	Personnels p5=new Personnels.Builder(5, "BILL", " zemmoura").build();
+	      Personnels p7=new Personnels.Builder(7, "riayd", " belhabib").build();
+	      Personnels p1=new Personnels.Builder(1, "BILL", " zemmoura").build();
+	      Personnels p2=new Personnels.Builder(2, "riayd", " belhabib").build();
+	      DAO<Personnels> pDAO=DAOFactory.getPersonnelDAO();
+		    pDAO.create(p1);
+		    pDAO.create(p2);
+		    pDAO.create(p5);
+		    pDAO.create(p7);
+		   p5.setNom("meriem");
+		   pDAO.update(p5);
+		   pDAO.delete(p7);
+		   load=personnelDAO.lire();
+	    
+	   System.out.println(load.size());
+	 
+	assertTrue(pDAO.find(p1)&&pDAO.find(p2)&&pDAO.find(p5));
+		 
+
+
+
+	   System.out.println("le nombre de composite est :"+load.size());
+	    }
+
+		
+	
+	@Test
 	public void afficheTest() {
-		Date date = new Date();
 		Personnels p4 = new Personnels.Builder(4, "BILL", " zemmoura").build();
 		Personnels p1 = new Personnels.Builder(1, " BILL", "zemmoura").build();
 		Personnels p5 = new Personnels.Builder(5, "BILL", "zemmoura").build();
@@ -114,12 +173,12 @@ public class test
 		p3.add(p1);
 		p3.add(p2);
 
-		AffichageGroupe a = new AffichageGroupe(p3);
+		/*AffichageGroupe a = new AffichageGroupe(p3);
 		Iterator i = a.getIterator();
 		while (i.hasNext()) {
 
 			i.Next().afficher();
-		}
+		}*/
 
 	}
 
